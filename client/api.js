345b56ff405bb9cc227b8f4a3d1f44b8026146db
Paul Brabet <pbrabet@gmail.com>
2017-12-13 05:07:43 +1300

    cut unnecessary ode from api.js

--- client/api.js
+++ client/api.js
@@ -3,20 +3,7 @@ import request from 'superagent'
 var widgetUrl = 'http://localhost:3000/widgets'
 
 module.exports = {
-  getWidgets: getWidgets,
   login: login
-}
-
-function getWidgets (callback) {
-  request
-    .get(widgetUrl)
-    .end(function (err, res) {
-      if (err) {
-        callback(err)
-      } else {
-        callback(null, res.body)
-      }
-    })
 }
 
 function login (callback) {
