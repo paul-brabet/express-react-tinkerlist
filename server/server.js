6083735e315bff633227f791894b3c606cd8f6d8
Paul Brabet <pbrabet@gmail.com>
2017-12-12 19:07:57 +1300

    Spotify returns to /api/v1/callback with data

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var cors = require('cors')
 var path = require('path')
+var cookieParser = require('cookie-parser')
 
 var apiRouter = require('./routes/api-routes')
 
 var server = express()
 
+server.use(cookieParser())
 server.use('/api/v1', apiRouter)
 server.use(bodyParser.json())
-server.use(cors({origin: 'http://localhost:8080'}))
 server.use(express.static(path.join(__dirname, '../public')))
 
 module.exports = server
