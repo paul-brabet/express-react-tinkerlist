18a85a226a5ec56ff5def9bb442a681b597f7e11
Paul Brabet <pbrabet@gmail.com>
2017-12-15 17:36:45 +1300

    map from AllPlaylists page no longer throwing errors

---
+++ client/components/AllPlaylists.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {getUserPlaylists} from '../actions/actions'
+import Playlist from './Playlist'
+
+class AllPlaylists extends React.Component {
+  componentDidMount () {
+    this.props.getUserPlaylists()
+  }
+
+  render () {
+    const allPlaylists = this.props.allPlaylists
+    const loadingStatus = this.props.loading.loading
+
+    if(loadingStatus) {
+      return <h2>Loading...</h2>
+    }
+
+    return (
+      <div>
+        <h2>Playlists (temporary page)</h2>
+        {allPlaylists.map(function(item) {
+          return (
+            <div key={item.id}>
+              <Playlist name={item.name} />
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+    loading: state.loading,
+    allPlaylists: state.allPlaylists.allPlaylists
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    getUserPlaylists: () => dispatch(getUserPlaylists())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllPlaylists)
